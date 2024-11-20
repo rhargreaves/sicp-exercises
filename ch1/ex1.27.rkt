a227b8c3a1aa686c1873b614ef10240a65cc9586
Robert Hargreaves <kiyouta@gmail.com>
2024-11-20 23:53:44 +0000

    Complete ex1.27

---
+++ ch1/ex1.27.rkt
@@ -0,0 +1,37 @@
+#lang sicp
+
+(#%require rackunit
+           racket/trace)
+
+(define (square x) (* x x))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (carmichael-number? n)
+  (define (try-it n a)
+    (cond ((= a 1) #t)
+          ((not (= (expmod a n n) a)) #f)
+          (else (try-it n (- a 1)))))
+  (try-it n (- n 1)))
+
+(display (carmichael-number? 561))  (newline)
+(display (carmichael-number? 1105)) (newline)
+(display (carmichael-number? 1729)) (newline)
+(display (carmichael-number? 2465)) (newline)
+(display (carmichael-number? 2821)) (newline)
+(display (carmichael-number? 6601)) (newline)
