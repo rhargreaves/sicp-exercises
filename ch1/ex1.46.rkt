e21d05045e1e5cfa6c29dc6904367f5afb54d592
Robert Hargreaves <kiyouta@gmail.com>
2024-12-14 22:24:24 +0000

    Complete ex1.46 - and chapter 1! :tada:

--- ch1/ex1.46.rkt
+++ ch1/ex1.46.rkt
@@ -18,38 +18,43 @@
         (compose f (recur (- n 1)))))
   (recur n))
 
+;; (define (fixed-point f first-guess close-enough?)
+;;   (define (try guess)
+;;     (let ((next (f guess)))
+;;       (if (close-enough? guess next)
+;;           next
+;;           (try next))))
+;;   (try first-guess))
+
+(define (always-false guess next) #f)
+(define (always-true guess next) #t)
+(define (more-than-ten? guess next) (> guess 10))
+
+(define (iterative-improve good-enough? improve-guess)
+  (lambda (initial-guess)
+    (define (recur guess)
+      (let ((next (improve-guess guess)))
+        (if (good-enough? guess next)
+            next
+            (recur next))))
+    (recur initial-guess)))
+
+((iterative-improve more-than-ten? inc) 1)
+
 (define tolerance 0.00001)
 
 (define (close-enough? v1 v2)
   (< (abs (- v1 v2)) tolerance))
 
 (define (fixed-point f first-guess close-enough?)
-  (define (try guess)
-    (let ((next (f guess)))
-      (if (close-enough? guess next)
-          next
-          (try next))))
-  (try first-guess))
+  ((iterative-improve close-enough? f) first-guess))
 
 (define (average-damp f)
   (lambda (x) (average x (f x))))
-
+  
 (define (sqrt x)
   (fixed-point (average-damp (lambda (y) (/ x y)))
                1.0 close-enough?))
 
-(define (always-false x) #f)
-(define (always-true x) #t)
-(define (more-than-ten? x) (> x 10))
-
-(define (iterative-improve good-enough? improve-guess)
-  (lambda (initial-guess)
-    (define (recur guess)
-      (if (good-enough? guess)
-          guess
-          (recur (improve-guess guess))))
-    (recur initial-guess)))
-
-((iterative-improve more-than-ten? inc) 1)
-
+(sqrt 4)
                  
