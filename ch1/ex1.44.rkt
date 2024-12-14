baadc9a04a6838479ea9b9130368162f32749bb1
Robert Hargreaves <kiyouta@gmail.com>
2024-12-14 15:34:54 +0000

    Implemented first half of ex1.44

---
+++ ch1/ex1.44.rkt
@@ -0,0 +1,39 @@
+#lang sicp
+
+(#%require rackunit
+           racket/trace)
+
+(define (inc n) (+ n 1))
+(define (square x) (* x x))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(check-equal? ((compose square inc) 6) 49)
+
+(define (repeated f n)
+  (define (recur n)
+    (if (= n 1)
+        f
+        (compose f (recur (- n 1)))))
+  (recur n))
+
+(check-equal? ((repeated square 2) 5) 625)
+
+; ----
+
+(define dx 0.001)
+
+(define (smooth f)
+  (lambda (x) (/ (+ (f (- x dx))
+                    (f x)
+                    (f (+ x dx))) 3)))
+
+(check-equal? ((smooth square) 3) 9.000000666666667)
+; (tan 5)
+(check-equal? ((smooth tan) 5) -3.3805430150124103)
+
+; ----
+
+
+
