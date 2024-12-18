29a721419b9a8e82e7c59c7dd7127d36ed37432a
Robert Hargreaves <kiyouta@gmail.com>
2024-12-18 15:41:08 +0000

    Add tests

--- ch2/ex2.6.rkt
+++ ch2/ex2.6.rkt
@@ -62,3 +62,23 @@
   (lambda (f)
     (lambda (x)
       ((m f) ((n f) x)))))
+
+; key to lambda calculus is that the value of the "number" is the
+; number of times that the function (f) is applied to x.
+; So 0 = no applications.
+; 1 = one application (f x)
+; 2 = two apps (f (f x)
+
+; can use a function that increments an integer to test the applications of
+; functions:
+
+(define (inc n)
+  (+ 1 n))
+
+(check-equal? ((zero inc) 0) 0)
+(check-equal? ((one inc) 0) 1)
+(check-equal? ((two inc) 0) 2)
+
+(check-equal? (((add zero one) inc) 0) 1)
+(check-equal? (((add one one) inc) 0) 2)
+(check-equal? (((add two two) inc) 0) 4)
