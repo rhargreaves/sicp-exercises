8158a5f0bf8cb6caf7bb05a14b5346b252e99495
Robert Hargreaves <kiyouta@gmail.com>
2024-12-18 00:08:35 +0000

    Complete ex2.5

---
+++ ch2/ex2.5.rkt
@@ -0,0 +1,33 @@
+#lang sicp
+
+(#%require rackunit
+           racket/trace)
+
+(define (cons a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define (car z)
+  (define (iter remaining result)
+    (if (= 0 (remainder remaining 2))
+        (iter (/ remaining 2) (+ result 1))
+        result))
+  (iter z 0))
+
+(define (cdr z)
+  (define (iter remaining result)
+    (if (= 0 (remainder remaining 3))
+        (iter (/ remaining 3) (+ result 1))
+        result))
+  (iter z 0))
+
+(check-equal? (cons 1 1) 6)
+(check-equal? (car (cons 1 1)) 1)
+(check-equal? (cdr (cons 1 1)) 1)
+
+(check-equal? (cons 2 2) 36)
+(check-equal? (car (cons 2 2)) 2)
+(check-equal? (cdr (cons 2 2)) 2)
+
+(check-equal? (cons 5 6) 23328)
+(check-equal? (car (cons 2 2)) 2)
+(check-equal? (cdr (cons 2 2)) 2)
