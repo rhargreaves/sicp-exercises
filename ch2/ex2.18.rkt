e8db0206aedf0cb8253bcb7e563f5de5853c2d75
Robert Hargreaves <kiyouta@gmail.com>
2024-12-29 22:00:36 +0000

    Implement iterative version This is more efficient (O(n) rather than O(n**2))

--- ch2/ex2.18.rkt
+++ ch2/ex2.18.rkt
@@ -8,13 +8,25 @@
       list2
       (cons (car list1) (append (cdr list1) list2))))
 
+(check-equal? (append '(1 2) '(3 4)) '(1 2 3 4))
+
 (define (reverse items)
   (if (null? items)
       nil
   (append (reverse (cdr items))
           (cons (car items) nil))))
 
-(check-equal? (append '(1 2) '(3 4)) '(1 2 3 4))
+
 (check-equal? (reverse '()) '())
 (check-equal? (reverse (list 1 2 3)) (list 3 2 1))
 (check-equal? (reverse (list 1 2 3 4)) (list 4 3 2 1))
+
+(define (rev-iter items)
+  (define (iter items acc)
+    (if (null? items)
+        acc
+        (iter (cdr items) (cons (car items) acc))))
+  (iter items '()))
+
+(check-equal? (rev-iter (list 1 2 3)) (list 3 2 1))
+(check-equal? (rev-iter (list 1 2 3 4)) (list 4 3 2 1))
