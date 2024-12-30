c462068e0cc4f4aaaa42e10f7d9ccc2471229674
Robert Hargreaves <kiyouta@gmail.com>
2024-12-30 15:05:00 +0000

    Refactor ex2.20 to use filter

--- ch2/ex2.20.rkt
+++ ch2/ex2.20.rkt
@@ -16,14 +16,16 @@
 (check-equal? (equal-parity? 3 3) #t)
 (check-equal? (equal-parity? 3 1) #t)
 
+(define (filter pred lst)
+  (cond ((null? lst) '())
+        ((pred (car lst))
+         (cons (car lst) (filter pred (cdr lst))))
+        (else (filter pred (cdr lst)))))
+
 (define (same-parity parity . rest)
-  (define (recur rest)
-    (if (null? rest)
-        nil
-        (if (equal-parity? parity (car rest))
-            (cons (car rest) (recur (cdr rest)))
-            (recur (cdr rest)))))
-  (cons parity (recur rest)))
+   (cons parity
+         (filter (lambda (n) (equal-parity? parity n))
+                 rest)))
 
 (check-equal? (same-parity 1 2 3 4 5 6 7) '(1 3 5 7))
 (check-equal? (same-parity 2 3 4 5 6 7) '(2 4 6))
