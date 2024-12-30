385f2a64af47d2f84c5e14e2eb2f768163d61792
Robert Hargreaves <kiyouta@gmail.com>
2024-12-30 17:20:34 +0000

    Complete ex2.26

---
+++ ch2/ex2.26.rkt
@@ -0,0 +1,18 @@
+#lang sicp
+
+(#%require rackunit
+           racket/trace)
+
+(define x (list 1 2 3))
+
+(define y (list 4 5 6))
+
+; (append x y)
+; (1 2 3 4 5 6)
+
+; (cons x y)
+; ((1 2 3) . (4 5 6)) ---- (wrong), actual:
+; ((1 2 3) 4 5 6)
+
+; (list x y)
+; ((1 2 3) (4 5 6))
