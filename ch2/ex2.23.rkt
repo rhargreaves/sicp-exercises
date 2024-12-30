3d1f22a7e63a4e06cf13e5645c1f8ecf06ccedcf
Robert Hargreaves <kiyouta@gmail.com>
2024-12-30 16:04:55 +0000

    Complete ex2.23

---
+++ ch2/ex2.23.rkt
@@ -0,0 +1,16 @@
+#lang sicp
+
+(#%require rackunit
+           racket/trace)
+
+(define (for-each proc items)
+  (if (not (null? items))
+      (begin
+        (proc (car items))
+        (for-each proc (cdr items)))))
+
+(for-each (lambda (x) (newline) (display x))
+          (list 57 321 88))
+
+
+            
